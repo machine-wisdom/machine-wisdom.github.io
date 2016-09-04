56c76282d21b99b4aca38fd2b735b0031e627ccc
Younes Abouelnagah <younes.abouelnagah@flipp.com>
2016-09-04 18:33:37 -0400

    Created a WWW folder and a blog folder

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,27 @@
 
 echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
-# Build the project.
+pushd $(dirname "$0") > /dev/null
+
+cd www
+
+# Build the www part
 hugo
+
+popd > /dev/null
+
+pushd $(dirname "$0") > /dev/null
+
+cd blog
+
+# Build the www part
+hugo
+
+popd > /dev/null
+
+mv www/public .
+
+mv blog/public public/blog
 
 # Add changes to git.
 git add -A
@@ -17,4 +36,5 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
+
 git subtree push --prefix=public git@github.com:machine-wisdom/machine-wisdom.github.io.git gh-pages
